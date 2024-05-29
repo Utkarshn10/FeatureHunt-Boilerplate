c4888af3e77d96dd1f2b0a1fc925c1ad2b072d09
Utkarsh Nagar <58587256+Utkarshn10@users.noreply.github.com>
2024-05-29 15:59:14 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex justify-center">
       <li className="flex flex-col space-y-4 w-4/5">
-      {Object.values(randomFeatures).map((featureInfo,index) => {
+      {Object.values(randomFeatures)?.map((featureInfo,index) => {
         return (
           <ul key={index}>
             <InfoCards props={featureInfo} />
